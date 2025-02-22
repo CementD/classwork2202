4486f62e3225bf9b87c569b0f82d4e312ba98609
Semen Domin <deminsem2008@gmail.com>
2025-02-22 16:49:35 +0100

    Add project files.

---
+++ classwork2202/Form1.Designer.cs
@@ -0,0 +1,437 @@
+﻿namespace classwork2202
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            seven = new PictureBox();
+            eight = new PictureBox();
+            nine = new PictureBox();
+            four = new PictureBox();
+            five = new PictureBox();
+            six = new PictureBox();
+            one = new PictureBox();
+            two = new PictureBox();
+            three = new PictureBox();
+            zero = new PictureBox();
+            point = new PictureBox();
+            equal = new PictureBox();
+            division = new PictureBox();
+            multiplication = new PictureBox();
+            subtraction = new PictureBox();
+            addition = new PictureBox();
+            cancel = new PictureBox();
+            ce = new PictureBox();
+            square = new PictureBox();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            minus = new PictureBox();
+            button1 = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)seven).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)eight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nine).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)four).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)five).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)six).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)one).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)two).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)three).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)zero).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)point).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)equal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)division).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)multiplication).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)subtraction).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)addition).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cancel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ce).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)square).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)minus).BeginInit();
+            SuspendLayout();
+            // 
+            // seven
+            // 
+            seven.Image = (Image)resources.GetObject("seven.Image");
+            seven.Location = new Point(24, 267);
+            seven.Name = "seven";
+            seven.Size = new Size(79, 62);
+            seven.SizeMode = PictureBoxSizeMode.StretchImage;
+            seven.TabIndex = 0;
+            seven.TabStop = false;
+            seven.Click += seven_Click;
+            // 
+            // eight
+            // 
+            eight.Image = (Image)resources.GetObject("eight.Image");
+            eight.Location = new Point(119, 267);
+            eight.Name = "eight";
+            eight.Size = new Size(79, 62);
+            eight.SizeMode = PictureBoxSizeMode.StretchImage;
+            eight.TabIndex = 1;
+            eight.TabStop = false;
+            eight.Click += eight_Click;
+            // 
+            // nine
+            // 
+            nine.Image = (Image)resources.GetObject("nine.Image");
+            nine.Location = new Point(213, 267);
+            nine.Name = "nine";
+            nine.Size = new Size(79, 62);
+            nine.SizeMode = PictureBoxSizeMode.StretchImage;
+            nine.TabIndex = 2;
+            nine.TabStop = false;
+            nine.Click += nine_Click;
+            // 
+            // four
+            // 
+            four.Image = (Image)resources.GetObject("four.Image");
+            four.Location = new Point(24, 348);
+            four.Name = "four";
+            four.Size = new Size(79, 62);
+            four.SizeMode = PictureBoxSizeMode.StretchImage;
+            four.TabIndex = 3;
+            four.TabStop = false;
+            four.Click += four_Click;
+            // 
+            // five
+            // 
+            five.Image = (Image)resources.GetObject("five.Image");
+            five.Location = new Point(119, 348);
+            five.Name = "five";
+            five.Size = new Size(79, 62);
+            five.SizeMode = PictureBoxSizeMode.StretchImage;
+            five.TabIndex = 4;
+            five.TabStop = false;
+            five.Click += five_Click;
+            // 
+            // six
+            // 
+            six.Image = (Image)resources.GetObject("six.Image");
+            six.Location = new Point(213, 348);
+            six.Name = "six";
+            six.Size = new Size(79, 62);
+            six.SizeMode = PictureBoxSizeMode.StretchImage;
+            six.TabIndex = 5;
+            six.TabStop = false;
+            six.Click += six_Click;
+            // 
+            // one
+            // 
+            one.Image = (Image)resources.GetObject("one.Image");
+            one.Location = new Point(24, 429);
+            one.Name = "one";
+            one.Size = new Size(79, 62);
+            one.SizeMode = PictureBoxSizeMode.StretchImage;
+            one.TabIndex = 6;
+            one.TabStop = false;
+            one.Click += one_Click;
+            // 
+            // two
+            // 
+            two.Image = (Image)resources.GetObject("two.Image");
+            two.Location = new Point(119, 429);
+            two.Name = "two";
+            two.Size = new Size(79, 62);
+            two.SizeMode = PictureBoxSizeMode.StretchImage;
+            two.TabIndex = 7;
+            two.TabStop = false;
+            two.Click += two_Click;
+            // 
+            // three
+            // 
+            three.Image = (Image)resources.GetObject("three.Image");
+            three.Location = new Point(213, 429);
+            three.Name = "three";
+            three.Size = new Size(79, 62);
+            three.SizeMode = PictureBoxSizeMode.StretchImage;
+            three.TabIndex = 8;
+            three.TabStop = false;
+            three.Click += three_Click;
+            // 
+            // zero
+            // 
+            zero.Image = (Image)resources.GetObject("zero.Image");
+            zero.Location = new Point(119, 506);
+            zero.Name = "zero";
+            zero.Size = new Size(79, 62);
+            zero.SizeMode = PictureBoxSizeMode.StretchImage;
+            zero.TabIndex = 9;
+            zero.TabStop = false;
+            zero.Click += zero_Click;
+            // 
+            // point
+            // 
+            point.Image = (Image)resources.GetObject("point.Image");
+            point.Location = new Point(24, 506);
+            point.Name = "point";
+            point.Size = new Size(79, 62);
+            point.SizeMode = PictureBoxSizeMode.StretchImage;
+            point.TabIndex = 10;
+            point.TabStop = false;
+            point.Click += point_Click;
+            // 
+            // equal
+            // 
+            equal.Image = (Image)resources.GetObject("equal.Image");
+            equal.Location = new Point(310, 506);
+            equal.Name = "equal";
+            equal.Size = new Size(79, 62);
+            equal.SizeMode = PictureBoxSizeMode.StretchImage;
+            equal.TabIndex = 11;
+            equal.TabStop = false;
+            equal.Click += equal_Click;
+            // 
+            // division
+            // 
+            division.Image = (Image)resources.GetObject("division.Image");
+            division.Location = new Point(310, 189);
+            division.Name = "division";
+            division.Size = new Size(79, 62);
+            division.SizeMode = PictureBoxSizeMode.StretchImage;
+            division.TabIndex = 12;
+            division.TabStop = false;
+            division.Click += division_Click;
+            // 
+            // multiplication
+            // 
+            multiplication.Image = (Image)resources.GetObject("multiplication.Image");
+            multiplication.Location = new Point(310, 267);
+            multiplication.Name = "multiplication";
+            multiplication.Size = new Size(79, 62);
+            multiplication.SizeMode = PictureBoxSizeMode.StretchImage;
+            multiplication.TabIndex = 13;
+            multiplication.TabStop = false;
+            multiplication.Click += multiplication_Click;
+            // 
+            // subtraction
+            // 
+            subtraction.Image = (Image)resources.GetObject("subtraction.Image");
+            subtraction.Location = new Point(310, 348);
+            subtraction.Name = "subtraction";
+            subtraction.Size = new Size(79, 62);
+            subtraction.SizeMode = PictureBoxSizeMode.StretchImage;
+            subtraction.TabIndex = 14;
+            subtraction.TabStop = false;
+            subtraction.Click += subtraction_Click;
+            // 
+            // addition
+            // 
+            addition.Image = (Image)resources.GetObject("addition.Image");
+            addition.Location = new Point(310, 429);
+            addition.Name = "addition";
+            addition.Size = new Size(79, 62);
+            addition.SizeMode = PictureBoxSizeMode.StretchImage;
+            addition.TabIndex = 15;
+            addition.TabStop = false;
+            addition.Click += addition_Click;
+            // 
+            // cancel
+            // 
+            cancel.Image = (Image)resources.GetObject("cancel.Image");
+            cancel.Location = new Point(213, 189);
+            cancel.Name = "cancel";
+            cancel.Size = new Size(79, 62);
+            cancel.SizeMode = PictureBoxSizeMode.StretchImage;
+            cancel.TabIndex = 16;
+            cancel.TabStop = false;
+            cancel.Click += cancel_Click;
+            // 
+            // ce
+            // 
+            ce.Image = (Image)resources.GetObject("ce.Image");
+            ce.Location = new Point(119, 189);
+            ce.Name = "ce";
+            ce.Size = new Size(79, 62);
+            ce.SizeMode = PictureBoxSizeMode.StretchImage;
+            ce.TabIndex = 17;
+            ce.TabStop = false;
+            ce.Click += ce_Click;
+            // 
+            // square
+            // 
+            square.Image = (Image)resources.GetObject("square.Image");
+            square.Location = new Point(24, 189);
+            square.Name = "square";
+            square.Size = new Size(79, 62);
+            square.SizeMode = PictureBoxSizeMode.StretchImage;
+            square.TabIndex = 18;
+            square.TabStop = false;
+            square.Click += square_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(24, 126);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(365, 38);
+            textBox1.TabIndex = 19;
+            textBox1.Text = "0";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            richTextBox1.Location = new Point(441, 126);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(244, 442);
+            richTextBox1.TabIndex = 20;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(441, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 38);
+            label1.TabIndex = 21;
+            label1.Text = "History";
+            // 
+            // minus
+            // 
+            minus.Image = (Image)resources.GetObject("minus.Image");
+            minus.Location = new Point(213, 506);
+            minus.Name = "minus";
+            minus.Size = new Size(79, 62);
+            minus.SizeMode = PictureBoxSizeMode.StretchImage;
+            minus.TabIndex = 22;
+            minus.TabStop = false;
+            minus.Click += minus_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(557, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 23;
+            button1.Text = "Save as";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(441, 81);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 24;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Calculator
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(697, 592);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(minus);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(square);
+            Controls.Add(ce);
+            Controls.Add(cancel);
+            Controls.Add(addition);
+            Controls.Add(subtraction);
+            Controls.Add(multiplication);
+            Controls.Add(division);
+            Controls.Add(equal);
+            Controls.Add(point);
+            Controls.Add(zero);
+            Controls.Add(three);
+            Controls.Add(two);
+            Controls.Add(one);
+            Controls.Add(six);
+            Controls.Add(five);
+            Controls.Add(four);
+            Controls.Add(nine);
+            Controls.Add(eight);
+            Controls.Add(seven);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Calculator";
+            Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)seven).EndInit();
+            ((System.ComponentModel.ISupportInitialize)eight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nine).EndInit();
+            ((System.ComponentModel.ISupportInitialize)four).EndInit();
+            ((System.ComponentModel.ISupportInitialize)five).EndInit();
+            ((System.ComponentModel.ISupportInitialize)six).EndInit();
+            ((System.ComponentModel.ISupportInitialize)one).EndInit();
+            ((System.ComponentModel.ISupportInitialize)two).EndInit();
+            ((System.ComponentModel.ISupportInitialize)three).EndInit();
+            ((System.ComponentModel.ISupportInitialize)zero).EndInit();
+            ((System.ComponentModel.ISupportInitialize)point).EndInit();
+            ((System.ComponentModel.ISupportInitialize)equal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)division).EndInit();
+            ((System.ComponentModel.ISupportInitialize)multiplication).EndInit();
+            ((System.ComponentModel.ISupportInitialize)subtraction).EndInit();
+            ((System.ComponentModel.ISupportInitialize)addition).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cancel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ce).EndInit();
+            ((System.ComponentModel.ISupportInitialize)square).EndInit();
+            ((System.ComponentModel.ISupportInitialize)minus).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox seven;
+        private PictureBox eight;
+        private PictureBox nine;
+        private PictureBox four;
+        private PictureBox five;
+        private PictureBox six;
+        private PictureBox one;
+        private PictureBox two;
+        private PictureBox three;
+        private PictureBox zero;
+        private PictureBox point;
+        private PictureBox equal;
+        private PictureBox division;
+        private PictureBox multiplication;
+        private PictureBox subtraction;
+        private PictureBox addition;
+        private PictureBox cancel;
+        private PictureBox ce;
+        private PictureBox square;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private PictureBox minus;
+        private Button button1;
+        private SaveFileDialog saveFileDialog1;
+        private Button button2;
+    }
+}
---
+++ classwork2202/Form1.cs
@@ -0,0 +1,236 @@
+using System.Globalization;
+namespace classwork2202
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "0";
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "1";
+            else
+                textBox1.Text = "1";
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "2";
+            else
+                textBox1.Text = "2";
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "3";
+            else
+                textBox1.Text = "3";
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "4";
+            else
+                textBox1.Text = "4";
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "5";
+            else
+                textBox1.Text = "5";
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "6";
+            else
+                textBox1.Text = "6";
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "7";
+            else
+                textBox1.Text = "7";
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "8";
+            else
+                textBox1.Text = "8";
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text += "9";
+            else
+                textBox1.Text = "9";
+        }
+        private void Operation(char op)
+        {
+            equal_Click(null, null);
+            textBox1.Text += op;
+        }
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+            Operation('+');
+        }
+
+        private void subtraction_Click(object sender, EventArgs e)
+        {
+            Operation('-');
+        }
+
+        private void multiplication_Click(object sender, EventArgs e)
+        {
+            Operation('*');
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            Operation('/');
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            string expression = textBox1.Text;
+            char[] operators = { '+', '-', '*', '/' };
+            int operatorIndex = -1;
+            char op = ' ';
+
+            for (int i = 1; i < expression.Length; i++)
+            {
+                if (operators.Contains(expression[i]))
+                {
+                    operatorIndex = i;
+                    op = expression[i];
+                    break;
+                }
+            }
+            if (operatorIndex == -1) return;
+
+            string left = "";
+            for (int i = 0; i < operatorIndex; i++)
+                left += expression[i];
+
+            string right = "";
+            for (int i = operatorIndex + 1; i < expression.Length; i++)
+                right += expression[i];
+
+            if (double.TryParse(left, NumberStyles.Any, CultureInfo.InvariantCulture, out double num1) &&
+                double.TryParse(right, NumberStyles.Any, CultureInfo.InvariantCulture, out double num2))
+            {
+                double result = 0;
+                if (op == '+') result = num1 + num2;
+                if (op == '-') result = num1 - num2;
+                if (op == '*') result = num1 * num2;
+                if (op == '/')
+                {
+                    if (num2 != 0) result = num1 / num2;
+                    else
+                    {
+                        MessageBox.Show("Division by zero!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                textBox1.Text = result.ToString().Replace(',', '.');
+                richTextBox1.Text += $"{expression}={result.ToString().Replace(',', '.')}\n";
+            }
+        }
+
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+        }
+
+        private void ce_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void square_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double num))
+            {
+                double result = num * num;
+                textBox1.Text = result.ToString();
+            }
+            else
+                MessageBox.Show("Error!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            char[] operators = { '+', '-', '*', '/' };
+            string[] parts = text.Split(operators);
+            string lastPart = parts[parts.Length - 1];
+
+            if (!lastPart.Contains(".") && lastPart.Length > 0)
+                textBox1.Text += ".";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = "";
+                saveFileDialog1.Title = "Save File";
+                saveFileDialog1.Filter = "Text File|*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = saveFileDialog1.FileName;
+                    File.WriteAllText(filePath, richTextBox1.Text);
+                    MessageBox.Show($"Saved to {filePath}");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect format!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "-";
+                return;
+            }
+
+            if (double.TryParse(textBox1.Text, out double num))
+            {
+                textBox1.Text = (-num).ToString();
+            }
+        }
+    }
+}
+
+
